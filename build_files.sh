4ac5905af48fb51e339c8f2056d621242c30a91b
shms0mms <shmsmms01@gmail.com>
2025-02-05 16:59:22 +0800

    fix: py -> python3.9

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 # Update pip
-py -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 
 # Install requirements
 pip install -r requirements.txt
 
 # Collect Static Files on Deploy
-py manage.py collectstatic
+python3.9 manage.py collectstatic
