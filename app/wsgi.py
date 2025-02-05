0d74a6d157b5cb45c349b6cdfda6fe9d76a3472e
shms0mms <shmsmms01@gmail.com>
2025-02-05 17:56:30 +0800

    fix: wsgi file

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 # Создание объекта приложения WSGI
 application = get_wsgi_application()
