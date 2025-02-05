8402d2271e34b1b513310ea674d5647ca51b6aec
shms0mms <shmsmms01@gmail.com>
2025-02-05 17:52:18 +0800

    chore: asgi & wsgi files has been changed

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for app project.
 
-It exposes the WSGI callable as a module-level variable named ``handler``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
-
-handler = application
