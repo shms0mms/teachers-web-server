87d3e4804ecb09d31bd5fe5d833f3d8f2acc7edb
shms0mms <shmsmms01@gmail.com>
2025-02-05 17:42:33 +0800

    fix: wsgi

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for app project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``handler``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
+
+handler = application
