0f217b604c5bdcd060ec68b18e9ddd9d99c6b2ec
shms0mms <shmsmms01@gmail.com>
2025-02-05 17:10:04 +0800

    chore: venv has been deleted & build_files.sh has been changed

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+which pip
+which python
+
 # Update pip
-python3.9 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Install requirements
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Collect Static Files on Deploy
-python3.9 manage.py collectstatic 
+python manage.py collectstatic 
