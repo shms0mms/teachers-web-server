fe957ec8b5cb5abaef5b2dd714c2f2a38df61ca2
shms0mms <shmsmms01@gmail.com>
2025-02-06 23:34:53 +0800

    chore: wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,9 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+print("Loading WSGI application...")  # Add this line
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+print("WSGI application loaded.")  # Add this line
