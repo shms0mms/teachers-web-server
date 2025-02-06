53ebc563c1a780437dd47053e1c468c6a8cf3b64
shms0mms <shmsmms01@gmail.com>
2025-02-06 23:31:44 +0800

    chore: wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -4,9 +4,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-# Создание объекта приложения WSGI
 application = get_wsgi_application()
-
-# Определение функции handler для Vercel
-def handler(event, context):
-    return application(event, context)
